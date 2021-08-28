29ed832eec2d66118b6cb460af163acca2317eb0
Lee Cha Min <45652743+ChaminLee@users.noreply.github.com>
2021-08-28 13:49:45 +0900

    Create Binary Tree Inorder Traversal.swift

---
+++ LeetCode/Tree/Binary Tree Inorder Traversal.swift
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        // root가 있는지 먼저 확인
+        guard let root = root else { return [] }
+        
+        // 좌측 회귀 + 중앙 + 우측 회귀 
+        return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
+    }
+}
