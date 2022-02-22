4901eeae76504e88a95346b7e0500eb074deb325
chacha <45652743+ChaminLee@users.noreply.github.com>
2022-02-23 02:25:21 +0900

    docs: 2609 최대공약수와 최소공배수 풀이 추가

--- BOJ/2609 최대공약수와 최소공배수.swift
+++ BOJ/2609 최대공약수와 최소공배수.swift
@@ -20,3 +20,22 @@ var minMum = maxNum * n * m
 
 print(maxNum)
 print(minMum)
+
+// GCD, LCD 공식 풀이
+
+import Foundation 
+
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+var n = input[0], m = input[1]
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    let temp = a % b
+    return temp == 0 ? min(a,b) : gcd(b, temp)
+}
+
+func lcd(_ a: Int, _ b: Int) -> Int {
+    return (a * b) / gcd(a,b)
+}
+
+print(gcd(n,m))
+print(lcd(n,m))
